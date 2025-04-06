64cb17c98b4585a14cf5be0199709ffe13b7cb62
Adnan <adnanali23189@gmail.com>
2025-04-06 07:19:09 +0530

    feat: Quantamania section tab arrangements

--- qubit-frontend-revamp/components/sections/Quantumania.tsx
+++ qubit-frontend-revamp/components/sections/Quantumania.tsx
@@ -16,6 +16,17 @@ const Quantumania = () => {
 
   const features = [
     {
+      title: "Jobs",
+      description: "Job Hunting made easy",
+      icon: (
+        <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 16.5v.75m3-3v3M6 13.5h.75m-1.5-3h6m-6 3h6m6-3a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+        </svg>
+      ),
+      link: "https://chat.whatsapp.com/CgMgHWj39wsCJSHZ9qzV4l",
+      image: "/assets/quantamania_job_im_showcase.jpg"
+    },
+    {
       title: "Courses",
       description: "Learn, upskill, and stay ahead with top-notch courses!",
       icon: (
@@ -47,17 +58,6 @@ const Quantumania = () => {
       ),
       link: "https://chat.whatsapp.com/Hv1LbQCGox9Itk6EM4CBuq",
       image: "/assets/quantamania_internships_im_showcase.jpg"
-    },
-    {
-      title: "Jobs",
-      description: "Job Hunting made easy",
-      icon: (
-        <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 16.5v.75m3-3v3M6 13.5h.75m-1.5-3h6m-6 3h6m6-3a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
-        </svg>
-      ),
-      link: "https://chat.whatsapp.com/CgMgHWj39wsCJSHZ9qzV4l",
-      image: "/assets/quantamania_job_im_showcase.jpg"
     }
   ];
 
